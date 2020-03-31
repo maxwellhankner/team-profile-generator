acbb3d65de3295afdcd6e4362583806a4af19b02
Maxwell Hankner <maxwellhankner@gmail.com>
2020-03-31 15:28:10 -0500

    pseudo coded app.js and installed inquirer

---
+++ app.js
@@ -0,0 +1,8 @@
+// Require packages
+
+// Create an empty array for the team members
+
+// Prompt user to build their team
+// One manager and as many interns and engineers as desired
+
+// Create a team.html file with the users team
